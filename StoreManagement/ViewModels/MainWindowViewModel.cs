e5c50a121afad6dcd1e744c3338213f426e8641a
Alexey Savelyev <savelyev.main@gmail.com>
2024-02-19 23:44:00 +0600

    тест привязки

--- StoreManagement/ViewModels/MainWindowViewModel.cs
+++ StoreManagement/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,11 @@ namespace StoreManagement.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
+        #region Заголовок главного окна
+        private string _title = "Управление магазином";
+        public string Title { get => _title; set => Set(ref _title, value); }
+        #endregion
+
         public MainWindowViewModel()
         {
             
--- StoreManagement/ViewModels/ViewModelRegistrator.cs
+++ StoreManagement/ViewModels/ViewModelRegistrator.cs
@@ -5,6 +5,6 @@ namespace StoreManagement.ViewModels
     static class ViewModelRegistrator
     {
         public static IServiceCollection AddViewModels(this IServiceCollection services) => services
-            .AddScoped<MainWindowViewModel>();
+            .AddSingleton<MainWindowViewModel>();
     }
 }
